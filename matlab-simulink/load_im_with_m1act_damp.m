42d2a120289c5799e86987a375ab42f5269a095f
Rodrigo Alvite Romano <rovite@gmail.com>
2025-08-11 12:02:18 -0300

    Splitting function to generate M1 act transmatrices and solving conflicts.

--- matlab-simulink/load_im_with_m1act_damp.m
+++ matlab-simulink/load_im_with_m1act_damp.m
@@ -27,7 +27,7 @@ osim.mnt_FFc_en = 1;    % [bool] Azimuth feedforward action switch
 % M1
 osim.m1olf_en = 1;      % [bool] M1 outer force loop switch
 osim.m1cpp_en = 0;      % [bool] M1 Command pre-processor activation flag
-osim.m1act_damp = 0;    % [numeric] 0: no damping, 1: linear, and 2: quadratic
+osim.m1act_damp = 1;    % [numeric] 0: no damping, 1: linear, and 2: quadratic
 % % M2
 % osim.m2PZT_en = 0;      % [bool] M2 PZT control loop switch
 % osim.m2_pos_en = 0;     % [bool] M2 Positioner control loop
@@ -460,95 +460,6 @@ rmpath(odc_base_util_folder, odc_base_conf_folder);
 
 end
 
-%% Function to calculate M1 actuator transformations
-function [M_oa_xyz2abc, M_cs_xyz2abc, M_oa_abc2xyz, M_cs_abc2xyz] =...
-    calc_ort2cylTs(m1_dt_folder)
-
-% OA
-load(fullfile(m1_dt_folder,'OA_SupportActuatorArrayConfig'),...
-    'OA_ActData','xyz2abc40','xyz2abc41','xyz2abc5',...
-    'abc2xyz40','abc2xyz41','abc2xyz5');
-
-nr = numel(find(OA_ActData(:,5) >= 40))*3 +...
-    numel(find(OA_ActData(:,5) > 5 & (OA_ActData(:,5) < 40))) +...
-    numel(find(OA_ActData(:,5) == 5))*6 +...
-    numel(find(OA_ActData(:,5) < 5));
-nc = nr - numel(find(OA_ActData(:,5) == 5))*3;
-fprintf("Dimension of Txyz2abc for OA segment: %dx%d\n",nr,nc);
-M_oa_xyz2abc = zeros(nr,nc);
-M_oa_abc2xyzT = zeros(nr,nc);
-i_cols = 1;
-i_rows = 1;
-for i_act = 1:size(OA_ActData,1)
-
-    if(OA_ActData(:,5) == 40)
-        M_oa_xyz2abc((0:2)+i_rows, (0:2)+i_cols) = xyz2abc40;
-        M_oa_abc2xyzT((0:2)+i_rows, (0:2)+i_cols) = abc2xyz40';
-        i_cols = i_cols+3;
-        i_rows = i_rows+3;
-    elseif(OA_ActData(:,5) == 41)
-        M_oa_xyz2abc((0:2)+i_rows, (0:2)+i_cols) = xyz2abc41;
-        M_oa_abc2xyzT((0:2)+i_rows, (0:2)+i_cols) = abc2xyz41';
-        i_cols = i_cols+3;
-        i_rows = i_rows+3;
-    elseif(OA_ActData(:,5) == 5)
-        M_oa_xyz2abc((0:5)+i_rows, (0:2)+i_cols) = xyz2abc5;
-        M_oa_abc2xyzT((0:5)+i_rows, (0:2)+i_cols) = abc2xyz5';
-        i_cols = i_cols+3;
-        i_rows = i_rows+6;
-    else
-        M_oa_xyz2abc(i_rows,i_cols) = 1;
-        M_oa_abc2xyzT(i_rows,i_cols) = 1;
-        i_cols = i_cols+1;
-        i_rows = i_rows+1;
-    end
-end
-
-M_oa_abc2xyz = M_oa_abc2xyzT';
-
-% CS
-load(fullfile(m1_dt_folder,'CS_SupportActuatorArrayConfig'),...
-    'CS_ActData','xyz2abc40','xyz2abc41','xyz2abc5',...
-    'abc2xyz40','abc2xyz41','abc2xyz5');
-
-nr = numel(find(CS_ActData(:,5) >= 40))*3 +...
-    numel(find(CS_ActData(:,5) > 5 & (CS_ActData(:,5) < 40))) +...
-    numel(find(CS_ActData(:,5) == 5))*6 +...
-    numel(find(CS_ActData(:,5) < 5));
-nc = nr - numel(find(CS_ActData(:,5) == 5))*3;
-fprintf("Dimension of Txyz2abc for CS segment: %dx%d\n",nr,nc);
-M_cs_xyz2abc = zeros(nr,nc);
-M_cs_abc2xyzT = zeros(nr,nc);
-i_cols = 1;
-i_rows = 1;
-for i_act = 1:size(CS_ActData,1)
-
-    if(CS_ActData(:,5) == 40)
-        M_cs_xyz2abc((0:2)+i_rows, (0:2)+i_cols) = xyz2abc40;
-        M_cs_abc2xyzT((0:2)+i_rows, (0:2)+i_cols) = abc2xyz40';
-        i_cols = i_cols+3;
-        i_rows = i_rows+3;
-    elseif(CS_ActData(:,5) == 41)
-        M_cs_xyz2abc((0:2)+i_rows, (0:2)+i_cols) = xyz2abc41;
-        M_cs_abc2xyzT((0:2)+i_rows, (0:2)+i_cols) = abc2xyz41';
-        i_cols = i_cols+3;
-        i_rows = i_rows+3;
-    elseif(CS_ActData(:,5) == 5)
-        M_cs_xyz2abc((0:5)+i_rows, (0:2)+i_cols) = xyz2abc5;
-        M_cs_abc2xyzT((0:5)+i_rows, (0:2)+i_cols) = abc2xyz5';
-        i_cols = i_cols+3;
-        i_rows = i_rows+6;
-    else
-        M_cs_xyz2abc(i_rows,i_cols) = 1;
-        M_cs_abc2xyzT(i_rows,i_cols) = 1;
-        i_cols = i_cols+1;
-        i_rows = i_rows+1;
-    end
-end
-
-M_cs_abc2xyz = M_cs_abc2xyzT';
-
-end
 
 %% Function to load wind-load time series
 function [windload_dt, wl_demux] = load_WLdt(ModelFolder, dur)
